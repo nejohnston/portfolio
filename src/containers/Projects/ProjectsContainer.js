5f2d9ec73db2ee90e175d6a67a699299acce15ff
Nicholas Johnston <nicholas.johnston94@gmail.com>
2021-01-14 23:14:29 -0800

    removed api call to github, opted for importing local json to projectscontainer

--- src/containers/Projects/ProjectsContainer.js
+++ src/containers/Projects/ProjectsContainer.js
@@ -3,29 +3,37 @@ import axios from 'axios'
 
 import Projects from './Projects'
 
+import projectData from '../../config/projects.json'
+
 class ProjectsContainer extends Component {
   constructor(props){
     super(props)
     this.state = { 
-      projects: [],
+      projects: [projectData],
       requested: ["covid-map", "boomtown", "building-assets", "freestyle"]
     }
   }
 
 
   componentDidMount() {
-      axios
-        .get(`https://api.github.com/users/nejohnston/repos`)
-        .then((resp) => resp.data)
-        .then(projectsList => {
-          this.setState({projects: projectsList})
-        });
+    // console.log(JSON.parse(projectData))
+      // fetch('../../config/projects.json', {
+      //   headers : { 
+      //     'Content-Type': 'application/json',
+      //     'Accept': 'application/json'
+      //    }})
+      //   .then(response => response.json())
+      //   .then(data => console.log(data))
+        // .catch((err) => {
+        //   // Do something for an error here
+        //   console.log('error')
+        // })
   }
   
     render() {
       console.log(this.state.projects)
         return (
-            <Projects projects={this.state.projects} />
+            <Projects projects={projectData}/>
         );
     }
 }
