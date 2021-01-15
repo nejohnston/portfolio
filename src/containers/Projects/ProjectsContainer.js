893bdf6f766c9a96af7436611dc2bd2c0578309e
Nicholas Johnston <nicholas.johnston94@gmail.com>
2021-01-14 23:52:16 -0800

    removed state from projects container, added github icon

--- src/containers/Projects/ProjectsContainer.js
+++ src/containers/Projects/ProjectsContainer.js
@@ -6,32 +6,8 @@ import Projects from './Projects'
 import projectData from '../../config/projects.json'
 
 class ProjectsContainer extends Component {
-  constructor(props){
-    super(props)
-    this.state = { 
-      projects: [projectData],
-      requested: ["covid-map", "boomtown", "building-assets", "freestyle"]
-    }
-  }
-
-
-  componentDidMount() {
-    // console.log(JSON.parse(projectData))
-      // fetch('../../config/projects.json', {
-      //   headers : { 
-      //     'Content-Type': 'application/json',
-      //     'Accept': 'application/json'
-      //    }})
-      //   .then(response => response.json())
-      //   .then(data => console.log(data))
-        // .catch((err) => {
-        //   // Do something for an error here
-        //   console.log('error')
-        // })
-  }
   
     render() {
-      console.log(this.state.projects)
         return (
             <Projects projects={projectData}/>
         );
